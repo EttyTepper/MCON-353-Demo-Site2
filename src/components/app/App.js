9c109a49855f6df46bca1a3f88bfa905d6e04d41
Etty Tepper <etepper@student.touro.edu>
2022-02-27 11:02:33 -0500

    App Header created with ability to switch pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,16 +1,32 @@
 import './App.css';
 
 import * as React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import {Home} from '../home/home';
 import {Todo} from '../todo/todo';
+import {Header} from '../header/header';
 
-
+// const TodoContext = React.createContext();
 function App() {
   return (
-    <div>
-   {/* <Home/>*/}
+    <div> 
+   
+   <BrowserRouter>
+   <Header />
+    <Routes>
+      <Route path="/" element={<Home/>}></Route> {/*Route is top level domain*/}
+      <Route path="/todo" element={<Todo />}> {/*This is an 'or' so either the Home pg will show or todo */}
+      {/* if the two paths would be nested, both would show up at the same time, not what we want right now.
+       sometimes you will want that like when you want the header to appear on every page */}
+      </Route>
+    </Routes>
+  </BrowserRouter>
+    
 
-    <Todo/>
     </div>
     
   );
